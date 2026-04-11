cfc96c40806cb03ffe6678a1c9af13ce717748d4
wxqmerge <wxqmerge@outlook.com>
2026-04-11 12:17:27 -0700

    unused files

--- src/services/fix.ps1
+++
@@ -1,5 +0,0 @@
-$content = Get-Content "dataService.ts" -Raw
-$oldPattern = "localStorage.setItem('ladder_players',"
-$newPattern = 'localStorage.setItem(getKeyPrefix() + "'"'"ladder_players"'"'"'"'",' 
-$content = $content -replace [regex]::Escape($oldPattern), $newPattern
-Set-Content "dataService.ts" $content
