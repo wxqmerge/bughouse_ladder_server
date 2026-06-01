91aca8c23de08c5fee0b680cedceabdc126986a6
wxqmerge <wxqmerge@outlook.com>
2026-05-31 18:00:21 -0700

    Change Clear Cell shortcut to Ctrl+I

--- src/components/ErrorDialog.tsx
+++ src/components/ErrorDialog.tsx
@@ -1725,7 +1725,7 @@ Enter_Recalculate_Save (Ctrl+E)
                       : "white",
                   }}
                 >
-                  Clear All Matching Cells (Ctrl+Q)
+                  Clear All Matching Cells (Ctrl+I)
                 </button>
                 <button
                   type="button"
