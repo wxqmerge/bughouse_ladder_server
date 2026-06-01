35574fef2fdea04bf3dd6713aed64c471441549a
wxqmerge <wxqmerge@outlook.com>
2026-05-31 18:06:35 -0700

    Revert Clear Cell shortcut to Ctrl+C

--- src/components/ErrorDialog.tsx
+++ src/components/ErrorDialog.tsx
@@ -1725,7 +1725,7 @@ Enter_Recalculate_Save (Ctrl+E)
                       : "white",
                   }}
                 >
-                  Clear All Matching Cells (Ctrl+Shift+X)
+                  Clear All Matching Cells (Ctrl+C)
                 </button>
                 <button
                   type="button"
